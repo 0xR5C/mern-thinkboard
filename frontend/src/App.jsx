666f0dd5b6eb7650463797e304ab43f574d09d78
0xr5c <foths72@gmail.com>
2025-08-19 11:02:38 +0300

    setup frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailsPage from './pages/NoteDetailsPage'
+
+const App = () => {
+  return (
+    <div data-theme="coffee" className="min-h-screen">
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailsPage />} />
+      </Routes>
+    </div>
+  )
+};
+
+export default App;
